e0dee62c1eb34b3cba3146ff9816869df5478ed9
ilentt <ilentt@ilenlab.com>
2015-08-12 13:41:57 +0700

    update class

--- HelloMaven/src/main/java/ilen/CheckNumeric.java
+++ HelloMaven/src/main/java/ilen/CheckNumeric.java
@@ -15,10 +15,12 @@ public class CheckNumeric {
 		// TODO Auto-generated method stub
 		String strText1 = "01234";
 		String strText2 = "0a1234";
+		String strText3 = "001234";
 		CheckNumeric check  = new CheckNumeric();
 		
 		System.out.println(strText1 + " is a numeric: " + check.getCheckNumeric(strText1));
 		System.out.println(strText2 + " is a numeric: " + check.getCheckNumeric(strText2));
+		System.out.println(strText3 + " is a numeric: " + check.getCheckNumeric(strText3));
 	}
 
 }
